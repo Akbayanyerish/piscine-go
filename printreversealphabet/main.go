25f3954c93f050907c165e684fe44baf96da6e32
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-16 19:56:49 +0600

    reverse

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -4,10 +4,10 @@ import "github.com/01-edu/z01"
 
 func main() {
 
-	for i := 122; i >= 97; i = i - 1 {
-		z01.PrintRune(rune(i))
+	for i := 'z'; i >= 'a'; i-- {
+		z01.PrintRune(i)
 
 	}
-	z01.PrintRune(32)
+	z01.PrintRune(10)
 
 }
