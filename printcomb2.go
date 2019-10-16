ff6426e56ef6a3eadee811ba3c43dc646c9cbde4
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-16 22:53:42 +0600

    2222

---
+++ printcomb2.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main (){
+
+	func PrintComb() {
+		for i := '0'; i <= '9'; i++ {
+
+			for j := '0'; j <= '9'; j++ {
+				
+				for k:='0'; k<='9'; k++ {
+			
+					for l:='0'; l<='9'; l++ {
+
+						z01.PrintRune(l)
+						z01.PrintRune(k)
+						z01.PrintRune(44)
+						z01.PrintRune(j)
+						z01.PrintRune(i)
+						}
+				}	
+
+			}
+		}
+	}
+	z01.PrintRune(10)
+}
