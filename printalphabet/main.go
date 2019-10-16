3f1ea8f78c99607d21afdd8c801eb940d5821d94
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-16 19:08:56 +0600

    alphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,14 +2,11 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main (){
-for i:=97; i<=122; i++ {
-z01.PrintRune (rune(i))
+func main() {
+	for i := 97; i <= 122; i++ {
+		z01.PrintRune(rune(i))
 
-}
-z01.PrintRune ('\n')
-
-
-
+	}
+	z01.PrintRune('\n')
 
 }
