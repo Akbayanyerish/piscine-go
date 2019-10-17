4a229440fa532cd2de83b7cf4262bbbdb2a96a65
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-17 23:04:24 +0600

    strlen

--- strlen.go
+++ strlen.go
@@ -1,9 +1,12 @@
 package piscine
 
 func StrLen(str string) int {
+
+	k := 0
 	for index, element := range str {
 
-		index := index + 1
+		index = index + 1
+		k = index
 	}
-	return index
+	return k
 }
