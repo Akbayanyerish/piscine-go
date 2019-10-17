65273b25af1fc92f3543f8cebc1227bec3f2beb1
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-18 00:08:38 +0600

    strrev

---
+++ strrev.go
@@ -0,0 +1,17 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func StrRev(s string) string {
+	j := 0
+	var strrune = []rune(s)
+	var s2 string
+	for index := range strrune {
+		j = index
+	}
+	j++
+	for i := j - 1; i >= 0; i-- {
+		s2 = s2 + string(strrune[i])
+	}
+	return s2
+}
