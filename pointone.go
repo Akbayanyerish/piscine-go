8b83c4f1c72eb767f068b6d9f75611cd331c8190
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-17 20:45:56 +0600

    pointer

--- pointone.go
+++ pointone.go
@@ -1,12 +1,10 @@
 package piscine
-
 import "fmt"
-
-func PointOne(n *int) {
-	*n = 1
-}
-func Piscine() {
-	n := 0
-	piscine.PointOne(&n)
-	fmt.Println(n)
+func PointOne(n *int){
+*n=1
+}  
+func piscine() {
+    n := 0
+  piscine.PointOne(&n)
+    fmt.Println(n)
 }
