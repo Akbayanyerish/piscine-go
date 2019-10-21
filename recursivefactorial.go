243bbc78612fea86e416b5d13bc6210736276004
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-21 20:20:34 +0600

    sss

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,13 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
+	result := 0
 	if nb == 0 {
-		return 1
+		result = 1
 	} else if nb < 0 || nb > 20 {
-		return 0
+		result = 0
 	} else if nb > 1 {
-		return nb * RecursiveFactorial(nb-1)
+		result = nb * RecursiveFactorial(nb-1)
 	}
+	return result
 }
