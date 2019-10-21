062e2f0ba27c702a5c171f9ef272d3bc1f050fde
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-21 21:16:03 +0600

    ddd

--- fibonacci.go
+++ fibonacci.go
@@ -3,7 +3,8 @@ package piscine
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
-	} else if index <= 1 {
+	}
+	if index <= 1 {
 		return 1
 	}
 	return Fibonacci(index-2) + Fibonacci(index-1)
