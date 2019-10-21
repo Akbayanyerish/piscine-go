6f0b6d7e28083d4fd7e3f6fe3f8f018804df2d54
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-21 19:58:54 +0600

    dfff

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,11 +5,11 @@ func IterativeFactorial(nb int) int {
 	if nb == 0 {
 		result := 1
 	} else if nb < 0 {
-		result:=0 
+		result := 0
 	} else {
 		for i := 1; i <= nb; i++ {
 			result := result * i
-		} 
+		}
 	}
-return result
+	return result
 }
