b08d1c5bc62b917fca74f1cae7dac9074538cad7
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-21 20:06:37 +0600

    ddd

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,12 +3,12 @@ package piscine
 func IterativeFactorial(nb int) int {
 	result := 1
 	if nb == 0 {
-		result := 1
+		result = 1
 	} else if nb < 0 {
-		result := 0
+		result = 0
 	} else {
 		for i := 1; i <= nb; i++ {
-			result := result * i
+			result = result * i
 		}
 	}
 	return result
