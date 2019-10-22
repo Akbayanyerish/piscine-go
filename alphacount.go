3aa84da5b4a9d8a2f054f6c68c266f950a59f56d
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-22 20:21:45 +0600

    fff

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,13 @@
 package piscine
+
 import "fmt"
-func AlphaCount(str string) int {
-k:=0
-for _, letter := range str{
-if (letter >= 'a' && letter <= 'z') || (letter>= 'A' && letter <= 'Z'){
-	k++
-}
-return k
-}
+	func AlphaCount(str string) int {
+		k:=0
+		for _, letter := range str{
+		if (letter >= 'a' && letter <= 'z') || (letter>= 'A' && letter <= 'Z'){
+		k++
+	}
+	return k
+	}
 }
 
