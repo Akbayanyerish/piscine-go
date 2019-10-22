114ec2f7c0aa6f2f84c53ab6276eef157111641e
akbayanyerish <akbayan.yerishova@nu.edu.kz>
2019-10-22 20:45:01 +0600

    aaa

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(str string) int {
 	k := 0
 	strrune := []rune(str)
-	for letter := range strrune {
+	for _, letter := range strrune {
 		if (letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122) {
 			k++
 		}
